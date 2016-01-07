e65d5f12d3b886edc56abb515f6666c3512cda90
Renee CS <voiceofrae@gmail.com>
2016-01-06 19:06:10 -0800

    removed uneccesesary files/code

--- dist/scripts/MainCtrl.js
+++ dist/scripts/MainCtrl.js
@@ -1,12 +1,10 @@
-blocChat.controller('MainCtrl', function ($scope, Rooms, Messages, $rootScope) {
+blocChat.controller('MainCtrl', function ($scope, Rooms, $rootScope) {
 
   $scope.title = "Bloc Chat";
 	
 	$scope.roomTitle = "Select a Room";
 
 	$scope.rooms = Rooms.all;
-	
-	$scope.messages = Messages.all;
 
 	// adapted from BlocItOff
 	$rootScope.$watch("currentRoom", function (room) {
@@ -15,16 +13,19 @@ blocChat.controller('MainCtrl', function ($scope, Rooms, Messages, $rootScope) {
 		}
 	});
 	
+	// set the room - not yet active
 	$scope.setRoom = function (roomName) {
 		$scope.roomName = $scope.room.name;
 		$scope.messages = Messages.getRoomMessages(room);
 // getRoomMessages() not yet complete
 	};
 	
+	// create a new room
 	$scope.addRoom = function () {
 		Rooms.addRoom($scope.newRoomName)
 	};
 	
+	// send a new message - not yet active
 	$scope.sendMessage = function () {
 		Rooms.sendMessage($scope.messageContent)
 	};
